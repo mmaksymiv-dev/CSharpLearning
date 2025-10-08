2f473bbd46fb6712a6f0e051a383d894107fcdb1
Mykola Maskymiv <kolaymaksimiv@gmail.com>
2025-10-08 15:22:38 +0300

    Type Conversion

--- Fundamentals/PrimitiveTypes.cs
+++ Fundamentals/PrimitiveTypes.cs
@@ -5,6 +5,7 @@ public static class PrimitiveTypes
     public static void Start()
     {
         VariablesAndConstants();
+        TypeConversion();
     }
 
 
@@ -20,11 +21,40 @@ public static class PrimitiveTypes
         string firstName = "Mykola";
         bool isWorking = false;
 
+        const float Pi = 3.14f;
+
         Console.WriteLine(number); // Output: 2
         Console.WriteLine(count); // Output: 10
         Console.WriteLine(totalPrice); // Output: 20.95
         Console.WriteLine(character); // Output: A
         Console.WriteLine(firstName); // Output: Mykola
         Console.WriteLine(isWorking); // Output: false
+
+        Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue); // Output: 0 255
+    }
+
+    /// <summary>
+    /// Type Conversion
+    /// </summary>
+    private static void TypeConversion()
+    {
+        //byte b = 1;
+        //int i = b;
+        //Console.WriteLine(i);
+
+        int i = 1000;
+        byte b = (byte)i;
+        Console.WriteLine(b);
+
+        try
+        {
+            string str = "true";
+            bool cb = Convert.ToBoolean(str);
+            Console.WriteLine(cb);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("The number could not be converted to a byte.");
+        }
     }
 }
