4eb7f7d096c26004bd5ab76da7d00034494b98b9
Mykola Maskymiv <kolaymaksimiv@gmail.com>
2025-10-08 15:33:46 +0300

    Operators

--- Fundamentals/PrimitiveTypes.cs
+++ Fundamentals/PrimitiveTypes.cs
@@ -6,6 +6,7 @@ public static class PrimitiveTypes
     {
         VariablesAndConstants();
         TypeConversion();
+        Operators();
     }
 
 
@@ -44,17 +45,38 @@ public static class PrimitiveTypes
 
         int i = 1000;
         byte b = (byte)i;
-        Console.WriteLine(b);
+        Console.WriteLine(b); // Output: 232
 
         try
         {
             string str = "true";
             bool cb = Convert.ToBoolean(str);
-            Console.WriteLine(cb);
+            Console.WriteLine(cb); // Output: True
         }
         catch (Exception)
         {
             Console.WriteLine("The number could not be converted to a byte.");
         }
     }
+
+    /// <summary>
+    /// Operators
+    /// </summary>
+    private static void Operators()
+    {
+        var a = 10;
+        var b = 3;
+        var c = 4;
+
+        Console.WriteLine(a + b); // Output: 13
+        Console.WriteLine(a / b); // Output: 3
+        Console.WriteLine((float)a / (float)b); // Output: 3.3333
+
+        Console.WriteLine(a + b * c); // Output: 22
+        Console.WriteLine((a + b) * c); // Output: 52
+
+        Console.WriteLine(a > b); // Output: True
+        Console.WriteLine(a != b); // Output: True
+        Console.WriteLine(!(c > b || c == a)); //Output: False
+    } 
 }
