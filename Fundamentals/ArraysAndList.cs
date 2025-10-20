a85fd5514200a277675b21b41d5f7cb10b150b76
Mykola Maskymiv <kolaymaksimiv@gmail.com>
2025-10-20 13:40:42 +0300

    Lists

--- Fundamentals/ArraysAndList.cs
+++ Fundamentals/ArraysAndList.cs
@@ -5,6 +5,7 @@ public static class ArraysAndList
     public static void Start()
     {
         Arrays();
+        Lists();
     }
 
     private static void Arrays()
@@ -46,5 +47,38 @@ public static class ArraysAndList
         Console.WriteLine("Effect of Reverse()");
         foreach (var n in numbers)
             Console.WriteLine(n); // Output: 14 9 6 2 0 0
+    }
+
+    private static void Lists()
+    {
+        var numbers = new List<int>() { 1, 2, 3, 4 };
+
+        numbers.Add(1);
+        numbers.AddRange(new int[] { 5, 6, 7 });
+
+        foreach (var number in numbers)
+            Console.WriteLine(number); //Output: 1 2 3 4 1 5 6 7
+
+        Console.WriteLine();
+        Console.WriteLine("Index of 1: " + numbers.IndexOf(1)); // Output: 0
+        Console.WriteLine("Last index of 1: " + numbers.LastIndexOf(1)); // Outout: 4
+
+        Console.WriteLine("Count: " + numbers.Count); // Output: 8
+
+        numbers.Remove(1);
+        foreach (var number in numbers)
+            Console.WriteLine(number); // Output: 2 3 4 1 5 6 7
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] == 1)
+                numbers.Remove(numbers[i]);
+        }
+
+        foreach (var number in numbers)
+            Console.WriteLine(number); // Output: 2 3 4 5 6 7
+
+        numbers.Clear();
+        Console.WriteLine("Count: " + numbers.Count); // Output: Count:  0
     }
 }
